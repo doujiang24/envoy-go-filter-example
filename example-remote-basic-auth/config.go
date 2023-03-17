f1c49e654cd4b7f299f7431da4133bcf8dcf9b9b
doujiang24 <doujiang24@gmail.com>
2023-03-17 22:08:02 +0800

    remote basic auth.

---
+++ example-remote-basic-auth/config.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/api"
+	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/http"
+	"google.golang.org/protobuf/types/known/anypb"
+
+	xds "github.com/cncf/xds/go/xds/type/v3"
+)
+
+func init() {
+	http.RegisterHttpFilterConfigFactory("basic-auth", configFactory)
+	http.RegisterHttpFilterConfigParser(&parser{})
+}
+
+type config struct {
+	host string
+	port uint64
+}
+
+type parser struct {
+}
+
+func (p *parser) Parse(any *anypb.Any) interface{} {
+	configStruct := &xds.TypedStruct{}
+	if err := any.UnmarshalTo(configStruct); err != nil {
+		panic(err)
+	}
+
+	v := configStruct.Value
+	conf := &config{}
+	if host, ok := v.AsMap()["host"].(string); ok {
+		conf.host = host
+	}
+	if port, ok := v.AsMap()["port"].(uint64); ok {
+		conf.port = port
+	}
+	return conf
+}
+
+func (p *parser) Merge(parent interface{}, child interface{}) interface{} {
+	panic("TODO")
+}
+
+func configFactory(c interface{}) api.StreamFilterFactory {
+	conf, ok := c.(*config)
+	if !ok {
+		panic("unexpected config type")
+	}
+	return func(callbacks api.FilterCallbackHandler) api.StreamFilter {
+		return &filter{
+			callbacks: callbacks,
+			config:    conf,
+		}
+	}
+}
---
+++ example-remote-basic-auth/filter.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"strings"
+
+	"github.com/envoyproxy/envoy/contrib/golang/filters/http/source/go/pkg/api"
+	"net/http"
+)
+
+type filter struct {
+	callbacks api.FilterCallbackHandler
+	config    *config
+}
+
+// parseBasicAuth parses an HTTP Basic Authentication string.
+// "Basic QWxhZGRpbjpvcGVuIHNlc2FtZQ==" returns ("Aladdin", "open sesame", true).
+func parseBasicAuth(auth string) (username, password string, ok bool) {
+	const prefix = "Basic "
+	// Case insensitive prefix match. See Issue 22736.
+	if len(auth) < len(prefix) || !strings.EqualFold(auth[:len(prefix)], prefix) {
+		return "", "", false
+	}
+	c, err := base64.StdEncoding.DecodeString(auth[len(prefix):])
+	if err != nil {
+		return "", "", false
+	}
+	cs := string(c)
+	username, password, ok = strings.Cut(cs, ":")
+	if !ok {
+		return "", "", false
+	}
+	return username, password, true
+}
+
+// checkRemote checks the username and password against a remote server, through http request.
+func checkRemote(config *config, username, password string) bool {
+	headers := map[string]string{
+		"Content-Type": "application/json",
+	}
+	body := fmt.Sprintf(`{"username": "%s", "password": "%s"}`, username, password)
+	remote := "http://" + config.host + ":" + config.port + "/check"
+	resp, err := http.Post(config.remote, headers, body)
+	if err != nil {
+		return false
+	}
+	if resp.StatusCode != 200 {
+		return false
+	}
+	return true
+}
+
+func (f *filter) verify(header api.RequestHeaderMap) (bool, string) {
+	auth, ok := header.Get("authorization")
+	if !ok {
+		return false, "no Authorization"
+	}
+	username, password, ok := parseBasicAuth(auth)
+	if !ok {
+		return false, "invalid Authorization format"
+	}
+	fmt.Printf("got username: %v, password: %v\n", username, password)
+
+	if ok := checkRemote(f.config, username, password); !ok {
+		return false, "invalid username or password"
+	}
+	return true, ""
+}
+
+func (f *filter) DecodeHeaders(header api.RequestHeaderMap, endStream bool) api.StatusType {
+	go func() {
+		if ok, msg := f.verify(header); !ok {
+			// TODO: set the WWW-Authenticate response header
+			f.callbacks.SendLocalReply(401, msg, map[string]string{}, 0, "bad-request")
+			return
+		}
+		f.callbacks.Continue(api.Continue)
+	}
+	return api.Running
+}
+
+func (f *filter) DecodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
+	return api.Continue
+}
+
+func (f *filter) DecodeTrailers(trailers api.RequestTrailerMap) api.StatusType {
+	return api.Continue
+}
+
+func (f *filter) EncodeHeaders(header api.ResponseHeaderMap, endStream bool) api.StatusType {
+	return api.Continue
+}
+
+func (f *filter) EncodeData(buffer api.BufferInstance, endStream bool) api.StatusType {
+	return api.Continue
+}
+
+func (f *filter) EncodeTrailers(trailers api.ResponseTrailerMap) api.StatusType {
+	return api.Continue
+}
+
+func (f *filter) OnDestroy(reason api.DestroyReason) {
+}
+
+func main() {
+}
